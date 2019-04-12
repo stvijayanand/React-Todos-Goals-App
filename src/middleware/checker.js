444f7fa80ba598922b67b7dbad8e7b7ca2c24a6b
Vijayanand Settin <stvijayanand@hotmail.com>
2019-04-12 15:09:11 -0700

    fix: references path

--- src/middleware/checker.js
+++ src/middleware/checker.js
@@ -1,5 +1,5 @@
-import { ADD_TODO } from "./actions/todos";
-import { ADD_GOAL } from "./actions/goals";
+import { ADD_TODO } from "../actions/todos";
+import { ADD_GOAL } from "../actions/goals";
 
 const checker = store => next => action => {
   if (
