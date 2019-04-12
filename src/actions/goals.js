0c8253ac02929c2706d4b6aab122b5f341789b1d
Vijayanand Settin <stvijayanand@hotmail.com>
2019-04-12 13:39:02 -0700

    fix: import path

--- src/actions/goals.js
+++ src/actions/goals.js
@@ -1,42 +1,40 @@
-import API from 'goals-todos-api'
+import API from "goals-todos-api";
 
 export const ADD_GOAL = "ADD_GOAL";
 export const REMOVE_GOAL = "REMOVE_GOAL";
 
-
 function addGoalAction(goal) {
-    return {
-      type: ADD_GOAL,
-      goal
-    };
-  }
+  return {
+    type: ADD_GOAL,
+    goal
+  };
+}
 
-  function removeGoalAction(id) {
-    return {
-      type: REMOVE_GOAL,
-      id
-    };
-  }
+function removeGoalAction(id) {
+  return {
+    type: REMOVE_GOAL,
+    id
+  };
+}
 
-  
 export function handleAddGoal(name, cb) {
-    return dispatch => {
-      return API.saveGoal(name)
-        .then(goal => {
-          dispatch(addGoalAction(goal));
-          cb();
-        })
-        .catch(() => alert("There was an error. Try again."));
-    };
-  }
+  return dispatch => {
+    return API.saveGoal(name)
+      .then(goal => {
+        dispatch(addGoalAction(goal));
+        cb();
+      })
+      .catch(() => alert("There was an error. Try again."));
+  };
+}
 
 export function handleDeleteGoal(goal) {
-    return dispatch => {
-      dispatch(removeGoalAction(goal.id));
+  return dispatch => {
+    dispatch(removeGoalAction(goal.id));
 
-      return API.deleteGoal(goal.id).catch(() => {
-        dispatch(addGoalAction(goal));
-        alert("An error occurred. Try again.");
-      });
-    };
-  }
+    return API.deleteGoal(goal.id).catch(() => {
+      dispatch(addGoalAction(goal));
+      alert("An error occurred. Try again.");
+    });
+  };
+}
