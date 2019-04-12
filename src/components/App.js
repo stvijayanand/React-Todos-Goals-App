d903dd60f3594df9889336140dfa5dc05220d35f
Vijayanand Settin <stvijayanand@hotmail.com>
2019-04-12 13:56:37 -0700

    feat: components folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ConnectedTodos from "./Todos";
+import ConnectedGoals from "./Goals";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
 
 class App extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+
+    dispatch(handleInitialData());
+  }
   render() {
+    if (this.props.loading === true) {
+      return <h3>Loading</h3>;
+    }
+
     return (
-      <div className="App">
-        Hello World!
+      <div>
+        <ConnectedTodos />
+        <ConnectedGoals />
       </div>
     );
   }
 }
 
-export default App;
+export default connect(state => ({
+  loading: state.loading
+}))(App);
